5660a33f01de8fed5f5f868637ff441962765687
João Martins <jmbaltazarmartins@gmail.com>
2024-01-02 18:58:35 +0000

    ficheiro de criacao de fichs deleted

--- LogicLanguage/createtests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p testsfiles
-
-echo "var x = TRUE;" > testsfiles/test_var_declaration.txt
-echo -e "var x = TRUE;\nx = FALSE;" > testsfiles/test_var_reassignment.txt
-echo "var x = TRUE OR FALSE;" > testsfiles/test_var_expression.txt
-echo -e "var x = TRUE;\nvar y = NOT x;" > testsfiles/test_var_usage.txt
-echo -e "var x = TRUE;\nvar y = FALSE;\nvar z = x AND y;" > testsfiles/test_var_multiple.txt
